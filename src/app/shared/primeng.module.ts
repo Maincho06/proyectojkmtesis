d5ff5b892864d07dec5e76e1c09c3dcf18e3697f
Mauricio06Everis <mauricioandre.huacchapacheco@nttdata.everis.com>
2021-10-13 18:31:50 -0500

    corrección de diseño

--- src/app/shared/primeng.module.ts
+++ src/app/shared/primeng.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import {SidebarModule} from 'primeng/sidebar';
 import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
 
 
 const PRIMENG = [
   SidebarModule,
-  ButtonModule
+  ButtonModule,
+  TableModule
 ];
 
 @NgModule({
