b0a1ad1f970268e3d40acd1a05db2d3b4a829848
Mauricio06Everis <mauricioandre.huacchapacheco@nttdata.everis.com>
2021-10-13 19:25:56 -0500

    input example

--- src/app/shared/primeng.module.ts
+++ src/app/shared/primeng.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import {SidebarModule} from 'primeng/sidebar';
 import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
+import {InputTextModule} from 'primeng/inputtext';
 
 
 const PRIMENG = [
   SidebarModule,
   ButtonModule,
-  TableModule
+  TableModule,
+  InputTextModule
 ];
 
 @NgModule({
