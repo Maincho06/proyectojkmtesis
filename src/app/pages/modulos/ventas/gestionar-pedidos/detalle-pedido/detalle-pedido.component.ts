7b73c2253f6f97ee719aa84cdd12b33d472caec5
Mauricio06Everis <mauricioandre.huacchapacheco@nttdata.everis.com>
2021-11-15 15:35:38 -0500

    gestionar pedidos listo

--- src/app/pages/modulos/ventas/gestionar-pedidos/detalle-pedido/detalle-pedido.component.ts
+++ src/app/pages/modulos/ventas/gestionar-pedidos/detalle-pedido/detalle-pedido.component.ts
@@ -4,6 +4,7 @@ import { PedidoService } from '@services/pedido.service';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { ObvsService } from '@services/obvs.service';
 import { ActivatedRoute } from '@angular/router';
+import { EstadoGeneral } from '@models/generalmodel';
 
 @Component({
   selector: 'app-detalle-pedido',
@@ -15,7 +16,9 @@ export class DetallePedidoComponent implements OnInit {
   formPedido: FormGroup;
   pedido: any;
   idPedido: number;
+  isEstadoCreado: boolean = true;
   productos: any[];
+  listaEstadoPedido: EstadoGeneral[];
   precioTotal: number;
 
 
@@ -33,6 +36,7 @@ export class DetallePedidoComponent implements OnInit {
       this.idPedido = params.id;
     });
     await this.obtenerPedidoById(this.idPedido);
+    await this.obteneEstadoPedido();
     this.inicializarFormulario(this.pedido);
   }
 
@@ -41,37 +45,65 @@ export class DetallePedidoComponent implements OnInit {
       'codigo'        : [null,[Validators.required]],
       'solicitante'   : [null,[Validators.required]],
       'cliente'       : [null,[Validators.required]],
-      'fechaSolicitud': [null,[Validators.required]],
+      'fechaSolicitud': [ {value: null, disabled: true},[Validators.required]],
       'fechaEntrega'  : [null, [Validators.required]],
       'estado'        : [null,[Validators.required]],
     });
   }
   
   inicializarFormulario(pedido: any) {
-    console.log('PEDIDO FORM: ', pedido);
     this.formPedido.reset({
-      codigo        : pedido.codigo,
+      codigo        : pedido.codigoOrden,
       solicitante   : pedido.solicitante,
       cliente       : pedido.cliente,
       fechaSolicitud: new Date(pedido.fechaRegistro),
-      // fechaEntrega  : pedido.fechaEntrega,
+      fechaEntrega  : new Date(pedido.fechaEntrega),
       estado        : pedido.estado
     });
+    // if(!this.isEstadoCreado)  this.formPedido.get('fechaEntrega').disable();
   }
 
   async obtenerPedidoById(id: number) {
     try {
       this._obvsService.toogleSpinner();
       this.pedido = await this._pedidoService.getPedidoById(id);
+      console.log(this.pedido)
       this.productos = this.pedido.pedidos;
       this.precioTotal = this.pedido.precio;
-      console.log('Pedido: ', this.pedido);
+      this.isEstadoCreado = this.pedido.estado.id == 1 ? true : false;
     } catch (error) {
       console.error(error);
     } finally {
       this._obvsService.toogleSpinner();
     }
 
+  }
+
+  async obteneEstadoPedido() {
+    try {
+      this.listaEstadoPedido = await this._pedidoService.getEstadoPedido();
+    } catch (error) {
+      console.log('ERROR: ', error);
+    }
+  }
+
+  async actualizarPedido() {
+    try {
+      this._obvsService.toogleSpinner();
+      const idEstado = this.formPedido.get('estado').value.id;
+      const fechaEntrega = new Date(this.formPedido.get('fechaEntrega').value);
+
+
+      const respEstado = await this._pedidoService.updatePedidoEstado(this.idPedido, idEstado);
+      const respEntrega = await this._pedidoService.updatePedidoFechaEntrega(this.idPedido, fechaEntrega);
+
+      console.log('RESP ESTADO: ', respEstado);
+      console.log('RESP ENTREGA: ', respEntrega);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this._obvsService.toogleSpinner();
+    }
   }
 
 
---
+++ src/app/shared/models/generalmodel.ts
@@ -0,0 +1,9 @@
+export interface EstadoGeneral {
+    id: number;
+    descripcion: string;
+}
+
+export interface TipoGeneral {
+    id: number;
+    descripcion: string;
+}
